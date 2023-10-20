4c9db195a89cf1db042d46467b42c44e9ec00659
kirtinikunj <kirtinikunj28@.gmail>
2023-10-20 15:45:52 +0100

    'homework'

---
+++ src/homework_week_1/Programme_10_ProductOfTwoNumbers.java
@@ -0,0 +1,21 @@
+package homework_week_1;
+
+/**
+ * 10. Write a Java program that takes two numbers as input and display the
+ * product of two numbers.
+ * Test Data:
+ * Input first number: 25
+ * Input second number: 5
+ * Expected Output : 25 x 5 = 125
+ */
+public class Programme_10_ProductOfTwoNumbers {
+    public static void main(String[] args) {
+        //First Number declaration
+        int firstnumber = 25;
+        //Second Number declaration
+        int secondnumber = 5;
+        int produced = firstnumber * secondnumber;
+        //Expected output
+        System.out.println(firstnumber + " x " + secondnumber + " = " + produced);
+    }
+}
---
+++ src/homework_week_1/Programme_6_AmericanFlag.java
@@ -0,0 +1,24 @@
+package homework_week_1;
+
+/**
+ * 6.Write a java programme to print an American Flag on the screen.
+ */
+public class Programme_6_AmericanFlag {
+    public static void main(String [] args){
+        System.out.println("* * * * * * ==================================");
+        System.out.println(" * * * * *  ==================================");
+        System.out.println("* * * * * * ==================================");
+        System.out.println(" * * * * *  ==================================");
+        System.out.println("* * * * * * ==================================");
+        System.out.println(" * * * * *  ==================================");
+        System.out.println("* * * * * * ==================================");
+        System.out.println(" * * * * *  ==================================");
+        System.out.println("* * * * * * ==================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+    }
+}
